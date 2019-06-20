e3bbcd03fe46b3b84b306c16418cb2c21143be40
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2019-06-20 14:37:33 +0530

    //message model updated

--- app/src/main/java/com/ht/neighbourchat/models/Message.java
+++ app/src/main/java/com/ht/neighbourchat/models/Message.java
@@ -1,16 +1,39 @@
 package com.ht.neighbourchat.models;
 
-public class Message {
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
+@Entity
+public class Message {
+    @ColumnInfo(name = "user-id")
+    private String id;
+
+    @PrimaryKey
+    @ColumnInfo(name = "message-id")
     private String messageId;
+
+    @ColumnInfo(name = "message-content")
     private String messageContent;
+
+    @ColumnInfo(name = "sender")
     private String sender;
+
+    @ColumnInfo(name = "status")
     private int status;
 
     public String getMessageId() {
         return messageId;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getMessageContent() {
         return messageContent;
