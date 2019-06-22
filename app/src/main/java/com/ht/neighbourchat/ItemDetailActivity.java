b318e4e4927ce0da3838bcf7f89b6d0529b13b65
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2019-06-22 17:51:29 +0530

    //optimize imports

--- app/src/main/java/com/ht/neighbourchat/ItemDetailActivity.java
+++ app/src/main/java/com/ht/neighbourchat/ItemDetailActivity.java
@@ -1,5 +1,6 @@
 package com.ht.neighbourchat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,14 @@ import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ht.neighbourchat.db.AppDatabase;
+import com.ht.neighbourchat.db.DataHandler;
+import com.ht.neighbourchat.models.Message;
+import com.ht.neighbourchat.models.UserDoa;
+
+import java.util.List;
 
 /**
  * An activity representing a single Item detail screen. This
@@ -16,7 +25,9 @@ import android.view.MenuItem;
  * item details are presented side-by-side with a list of items
  * in a {@link ItemListActivity}.
  */
+
 public class ItemDetailActivity extends AppCompatActivity {
+    public static AppDatabase appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,9 @@ public class ItemDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
+
+        appDatabase = DataHandler.getAppDatabase();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +66,9 @@ public class ItemDetailActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
+//            getIntent().getStringExtra()
             Bundle arguments = new Bundle();
+
             arguments.putString(ItemDetailFragment.ARG_ITEM_ID,
                     getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
             ItemDetailFragment fragment = new ItemDetailFragment();
@@ -61,6 +77,7 @@ public class ItemDetailActivity extends AppCompatActivity {
                     .add(R.id.item_detail_container, fragment)
                     .commit();
         }
+
     }
 
     @Override
@@ -78,4 +95,6 @@ public class ItemDetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
